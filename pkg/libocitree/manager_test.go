0214dee52dfe3060ddfc5e384efe3f6d993ce00c
Alexandre Negrel <negrel.dev@protonmail.com>
2022-11-06 21:58:54 +0000

    test: add test for Manager.Add method

--- pkg/libocitree/manager_test.go
+++ pkg/libocitree/manager_test.go
@@ -1,8 +1,14 @@
 package libocitree
 
 import (
+	"encoding/base64"
+	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"syscall"
 	"testing"
+	"time"
 
 	"github.com/containers/common/pkg/config"
 	"github.com/containers/image/v5/types"
@@ -108,6 +114,124 @@ func TestManagerCheckout(t *testing.T) {
 	_ = clonedRepo
 }
 
+func TestManagerAdd(t *testing.T) {
+	manager, cleanup := newTestManager(t)
+	defer cleanup()
+
+	const repoName = "archlinux"
+	addFilepath, err := filepath.Abs("../../main.go")
+	require.NoError(t, err, "fail to canonicalize main.go filepath")
+
+	// Clone archlinux repository
+	err = manager.Clone(repoName)
+	require.NoError(t, err)
+
+	// mount helper function
+	mount := func(repoName string) (mountpoint string, umount func() error, stat func(string) (fs.FileInfo, error)) {
+		repo, err := manager.Repository(repoName)
+		require.NoError(t, err)
+		mountpoint, err = repo.Mount()
+		require.NoError(t, err)
+
+		return mountpoint, repo.Unmount, func(fname string) (fs.FileInfo, error) {
+			dirfs := os.DirFS(mountpoint).(fs.StatFS)
+			return dirfs.Stat(filepath.Base(addFilepath))
+		}
+	}
+
+	_, umount, stat := mount(repoName)
+	defer umount()
+
+	// Ensure file is absent
+	_, err = stat(addFilepath)
+	require.Error(t, err, "file is already present in image")
+	require.Contains(t, err.Error(), "no such file or directory")
+
+	for _, test := range []struct {
+		chown            string
+		chmod            string
+		message          string
+		expectedUid      uint32
+		expectedGid      uint32
+		expectedFileMode fs.FileMode
+	}{
+		{
+			chown:            "",
+			chmod:            "",
+			message:          "simple message",
+			expectedUid:      0,
+			expectedGid:      0,
+			expectedFileMode: 0644,
+		},
+		{
+			chown:            "root:root",
+			chmod:            "0777",
+			message:          randomCommitMessage(),
+			expectedUid:      0,
+			expectedGid:      0,
+			expectedFileMode: 0777,
+		},
+		{
+			chown:            "root:2000",
+			chmod:            "0421",
+			message:          randomCommitMessage(),
+			expectedUid:      0,
+			expectedGid:      2000,
+			expectedFileMode: 0421,
+		},
+		{
+			chown:            "2020:2000",
+			chmod:            "0321",
+			message:          randomCommitMessage(),
+			expectedUid:      2020,
+			expectedGid:      2000,
+			expectedFileMode: 0321,
+		},
+	} {
+		t.Run("", func(t *testing.T) {
+			// Add the file
+			err = manager.Add(repoName, "/", AddOptions{
+				Chown:   test.chown,
+				Chmod:   test.chmod,
+				Message: test.message,
+			}, addFilepath)
+			require.NoError(t, err)
+
+			// Mount repository
+			_, umount, stat = mount(repoName)
+			defer umount()
+
+			// Ensure file is present
+			fileInfo, err := stat(addFilepath)
+			require.NoError(t, err, "file is not in image")
+
+			// With the right permission
+			require.Equal(t, test.expectedFileMode, fileInfo.Mode(), "added file don't have the right permissions")
+			// And owner
+			fileStat := fileInfo.Sys().(*syscall.Stat_t)
+			require.Equal(t, test.expectedUid, fileStat.Uid)
+			require.Equal(t, test.expectedGid, fileStat.Gid)
+
+			repo, err := manager.Repository(repoName)
+			require.NoError(t, err)
+
+			// Check commit
+			commits, err := repo.Commits()
+			require.NoError(t, err, "failed to retrieve commits from repository")
+			require.Greater(t, len(commits), 0, "commits list is empty")
+			// commit message
+			require.Contains(t, commits[0].Comment(), test.message + "\n", "commit message doesn't contains expected substring")
+
+
+			// Check commit CreatedBy
+			require.Equal(t,
+				commits[0].CreatedBy(),
+				fmt.Sprintf("/bin/sh -c #(ocitree) ADD --chown=\"%v\" --chmod=\"%v\" %v /", test.chown, test.chmod, addFilepath),
+			)
+		})
+	}
+}
+
 func newTestManager(t *testing.T) (manager *Manager, cleanup func()) {
 	workdir, err := os.MkdirTemp("", "testStorageRuntime")
 	require.NoError(t, err)
@@ -148,4 +272,8 @@ func tmpdir() (string, error) {
 		}
 	}
 	return tmpdir, err
+}
+
+func randomCommitMessage() string {
+	return base64.StdEncoding.EncodeToString([]byte(time.Now().Format(time.RFC3339Nano)))
 }
